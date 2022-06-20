21f096a79ddbea69c0f9f1c81e633155f46f97e4
mailkaze <mailkaze@gmail.com>
2022-06-20 15:01:42 -0400

    creando componentes

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXLuEfbwH8_5KrLZZhdjCUVe5IldGBbPg",
+  authDomain: "serviciosdecuidado-72b78.firebaseapp.com",
+  projectId: "serviciosdecuidado-72b78",
+  storageBucket: "serviciosdecuidado-72b78.appspot.com",
+  messagingSenderId: "27725666319",
+  appId: "1:27725666319:web:2362df6233131226d7ab7a",
+  measurementId: "G-42J94ZL6H1"
+};
+
+// Initialize Firebase
+export const firebaseapp = initializeApp(firebaseConfig);
